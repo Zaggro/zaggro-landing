3dced4b56bb7068bd9d47b2726d5ff3f6f82ca08
Georg-Gert <georg.gert.tammeleht@gmail.com>
2022-12-06 10:10:53 +0200

    Add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,37 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import 'styles/index.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Zaggro</title>
+        <meta
+          name="description"
+          content="Zaggro is the leading liquidity aggregator that combines the liquidity of DEXs and lending protocols into a comprehensive and secure interface and APIs"
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
