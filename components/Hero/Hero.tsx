2b0a2340b08f4bf9e28692a524d884fe2f83bc7c
Georg-Gert <georg.gert.tammeleht@gmail.com>
2022-12-06 11:00:02 +0200

    Hero component

---
+++ components/Hero/Hero.tsx
@@ -0,0 +1,33 @@
+import clsx from 'clsx'
+import Button from 'components/Button/Button'
+import Typography from 'components/Typography/Typography'
+import styles from './Hero.module.scss'
+
+interface HeroProps {
+  className?: string
+}
+
+function Hero({ className }: HeroProps) {
+  return (
+    <header className={clsx(styles.root, className)}>
+      <Typography tag="h1" variant="display-md">
+        Best prices in DeFi
+        <br />
+        <div className={styles.highlighted}>for Traders & dApps</div>
+      </Typography>
+      <Typography tag="p" variant="p-lg" className={styles.text}>
+        We are the leading liquidity aggregator that combines the liquidity of
+        DEXs and lending protocols into a comprehensive and secure interface and
+        APIs
+      </Typography>
+      <div className={styles.buttons}>
+        <Button className={styles.button}>Build with us</Button>
+        <Button variant="secondary" className={styles.button}>
+          Explore the protocol
+        </Button>
+      </div>
+    </header>
+  )
+}
+
+export default Hero
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,25 @@
-import Head from 'next/head'
 import styles from 'styles/pages/home.module.scss'
-import Layout from 'components/Layout/Layout'
+import Hero from 'components/Hero/Hero'
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Zaggro</title>
-        <meta name="description" content="Zaggro" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Layout>
-        <section className={styles.section} id="the-protocol">
-          The Protocol
-        </section>
-        <section className={styles.section} id="integrations">
-          Integrations
-        </section>
-        <section className={styles.section} id="partners">
-          Partners
-        </section>
-        <section className={styles.section} id="audits">
-          Audits
-        </section>
-        <section className={styles.section} id="contact">
-          Contact
-        </section>
-      </Layout>
+    <div className={styles.root}>
+      <Hero />
+      <section className={styles.section} id="the-protocol">
+        The Protocol
+      </section>
+      <section className={styles.section} id="integrations">
+        Integrations
+      </section>
+      <section className={styles.section} id="partners">
+        Partners
+      </section>
+      <section className={styles.section} id="audits">
+        Audits
+      </section>
+      <section className={styles.section} id="contact">
+        Contact
+      </section>
     </div>
   )
 }
