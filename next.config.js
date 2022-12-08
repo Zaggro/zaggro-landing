06888ca3430cb51a0c6d4391bf0f326d7a92e1ad
Laurynas Antanavičius <laurynas.ant@gmail.com>
2022-12-08 23:52:33 +0700

    updated build config

--- next.config.js
+++ next.config.js
@@ -14,6 +14,9 @@ const nextConfig = {
     disableServerWebpackPlugin: true,
     disableClientWebpackPlugin: true,
   },
+  output: {
+    standalone: true,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
