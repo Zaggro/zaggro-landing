2b0336e47a3e8d317265ae6af0178c0487603955
Georg-Gert <georg.gert.tammeleht@gmail.com>
2022-12-08 16:17:57 +0200

    Fix header links alignment

--- components/Navbar/DesktopNav/DesktopNav.tsx
+++ components/Navbar/DesktopNav/DesktopNav.tsx
@@ -2,6 +2,7 @@ import clsx from 'clsx'
 import Image from 'next/image'
 import Button from 'components/Button/Button'
 import Typography from 'components/Typography/Typography'
+import useViewportSize from 'hooks/useViewportSize'
 import { headerLinks } from 'constants/links'
 import styles from './DesktopNav.module.scss'
 
@@ -12,29 +13,37 @@ interface DesktopNavProps {
 // TODO: Active style based on which element is in view
 // TODO: on scroll down, hide navbar | on scroll up, show navbar
 function DesktopNav({ className }: DesktopNavProps) {
+  const { isSpecialBreakpoint } = useViewportSize(1275)
+
   return (
     <nav className={clsx(styles.root, className)}>
-      <Image
-        src="/images/logo/zaggro@2.png"
-        alt="ZAGGRO logo"
-        width={300}
-        height={82}
-        className={styles.logo}
-      />
-      <ul className={styles.links}>
-        {headerLinks.map((link) => (
-          <li key={link.href}>
-            <a href={link.href}>
-              <Typography variant="menu-md" className={styles.link}>
-                {link.text}
-              </Typography>
-            </a>
-          </li>
-        ))}
-      </ul>
-      <Button href="#" className={styles.button}>
-        Launch App
-      </Button>
+      <div className={styles.container}>
+        <Image
+          src={
+            !isSpecialBreakpoint
+              ? '/images/logo/zaggro@2.png'
+              : '/images/logo/zaggro-no-text@2.png'
+          }
+          alt="ZAGGRO logo"
+          width={300}
+          height={82}
+          className={!isSpecialBreakpoint ? styles.logo : styles.noTextLogo}
+        />
+        <ul className={styles.links}>
+          {headerLinks.map((link) => (
+            <li key={link.href}>
+              <a href={link.href}>
+                <Typography variant="menu-md" className={styles.link}>
+                  {link.text}
+                </Typography>
+              </a>
+            </li>
+          ))}
+        </ul>
+        <Button href="#" className={styles.button}>
+          Launch App
+        </Button>
+      </div>
     </nav>
   )
 }
